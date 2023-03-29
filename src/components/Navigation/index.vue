95dce456741ae79d7c449614c5230ccf7cfc4670
lklksup <18916169070@163.com>
2023-03-29 23:39:21 +0800

    ...

--- src/components/Navigation/index.vue
+++ src/components/Navigation/index.vue
@@ -1,13 +1,10 @@
 <template>
-    <div></div>
-    </template>
+  <div></div>
+</template>
     
     <script setup lang="ts">
-    
-    
-    </script>
+</script>
     
     <style lang="less" scoped>
-    
-    </style>
+</style>
     
